c79eb36066bab8874474e41ea14e2f95b7a2dcb3
IbrahamAdams <mm1499853@gmail.com>
2025-01-29 15:02:33 +0300

    update file in project

--- routes/web.php
+++ routes/web.php
@@ -20,40 +20,6 @@ Route::get('/', [AuthController::class , 'login']);
 Route::post('/', [AuthController::class , 'auth_login']);
 Route::get('/logout', [AuthController::class , 'logout']);
 
-Route::group(['middleware', 'useradmin' ] ,function(){
-
-    Route::get('/panel/dashboard', [DashboardController::class , 'dashboard']);
-    // Role
-    Route::get('/panel/role', [RoleController::class , 'list']);
-    Route::get('/panel/role/add', [RoleController::class , 'add']);
-    Route::post('/panel/role/add', [RoleController::class , 'insert']);
-    Route::get('/panel/role/edit/{id}', [RoleController::class , 'edit']);
-    Route::post('/panel/role/edit/{id}', [RoleController::class , 'update']);
-    Route::get('/panel/role/delete/{id}', [RoleController::class , 'delete']);
-
-    // user
-    Route::get('/panel/user', [UserController::class , 'list']);
-    Route::get('/panel/user/add', [UserController::class , 'add']);
-    Route::post('/panel/user/add', [UserController::class , 'insert']);
-    Route::get('/panel/user/edit/{id}', [UserController::class , 'edit']);
-    Route::post('/panel/user/edit/{id}', [UserController::class , 'update']);
-    Route::get('/panel/user/delete/{id}', [UserController::class , 'delete']);
-
-    Route::prefix('languages')->group(function () {
-        Route::get('/', [LanguageController::class, 'index'])->name('languages.index');
-        Route::get('/create', [LanguageController::class, 'create'])->name('languages.create');
-        Route::post('/', [LanguageController::class, 'store'])->name('languages.store');
-        Route::get('/{id}/edit', [LanguageController::class, 'edit'])->name('languages.edit');
-        Route::put('/{language}', [LanguageController::class, 'update'])->name('languages.update');
-        Route::delete('/{language}', [LanguageController::class, 'destroy'])->name('languages.destroy');
-    });
-
-    Route::get('/languages', [LanguageController::class, 'fetchLanguages'])->name('languages.fetch.user');
-    Route::get('/language/{languageCode}', [LanguageController::class, 'setLanguage'])->name('language.set-current.user');
-
-
-    Route::resource('/panel/permissions', PermissionController::class);
-});
 
 
 Route::get('/dashboard', function () {
@@ -71,10 +37,6 @@ Route::get('auth/google/callback', [SocialiteController::class , 'googleAuthenti
 
 Route::get('auth/facebook', [SocialiteController::class, 'facebookLogin'])->name('auth.facebook');
 Route::get('auth/facebook-callback', [SocialiteController::class, 'facebookAuthentication'])->name('auth.facebook-callback');
-
-
-
-
 
 Route::get('auth/whatsapp', [SocialiteController::class, 'showWhatsAppForm'])->name('auth.whatsapp');
 Route::post('auth/whatsapp', [SocialiteController::class, 'WhatsAppLogin'])->name('auth.whatsapp.post');
